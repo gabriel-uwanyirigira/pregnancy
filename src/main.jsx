734251c31b22e9769b036b22dbbd14402d7c83c9
gabriel-uwanyirigira <uwanyirigiragabr@gmail.com>
2024-10-19 15:38:19 +0200

    updated again

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* another cahnge to the file */}
     <App />
   </React.StrictMode>,
 )
